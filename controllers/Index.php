0f2ba114edfb906b9e61db71ee261d5702b9f9c4
datagutt <datagutt@lekanger.no>
2013-06-09 00:32:33 +0200

    Add the stuff needed for page menus. Still needs support in theme

--- controllers/Index.php
+++ controllers/Index.php
@@ -4,9 +4,11 @@ namespace Hawalius\Controllers;
 class Index extends \Hawalius\Controller{
 	public function index($url = ''){
 		$post = $this->app->getModel('post');
+		$page = $this->app->getModel('page');
 		
 		$this->view->render('posts.html', [
-			'posts' => $post->many()
+			'posts' => $post->many(),
+			'pages' => $page->many()
 		]);
 	}
 }
